5fd2ac5bb52790774a4570e7ce82dd4c1f07ce61
Jeff Li <jeff.chun.yinli@gmail.com>
2020-08-25 11:48:37 -0400

    added unit test for saving a user company via company invite on sign up

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -28,6 +28,21 @@ RSpec.describe UsersController, type: :controller do
       expect(flash[:error]).to eq("Please complete all required info and try again")
       expect(response).to redirect_to(new_user_path)
     end
+
+    describe "joining company via company invite" do
+      it "adds the company from the invite to the list of companies the user has" do
+        company_invite = company_invites(:unused_invite)
+
+        post :create, params: { user: { name: "Joanne", email: "joanne@test.hoost" }, invite: {invite_type: "CompanyInvite", invite_code: company_invite.invite_code} }
+
+        expect(flash[:success]).to eq("Welcome! You are now a member of ACME Inc.")
+        expect(response).to redirect_to(user_path)
+
+        user = User.find_by(email: "joanne@test.hoost")
+        expect(user.companies.count).to eq(1)
+        expect(user.companies.first.name).to eq("ACME Inc.")
+      end
+    end  
   end
 
 end
